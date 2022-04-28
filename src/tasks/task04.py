dbdd71e6ce9a1538bf721812c1b6a91ae0eecf6e
studokim <studokim@gmail.com>
2022-04-28 23:19:06 +0300

    task 4.2

--- src/tasks/task04.py
+++ src/tasks/task04.py
@@ -1,7 +1,7 @@
 import json
 from src.tasks.common_lib import *
 from src.tasks.common_data import SOLE
-from src.tasks.task01_lib import compute_diff
+from src.tasks.task01_lib import calc_diff
 from src.tasks.task04_lib import *
 from src.tasks.task02_lib import print_diff
 
@@ -10,12 +10,12 @@ def situation():
     return ["Реализовать решение СЛАУ двумя итерационными методами:",
             "- методом простой итерации",
             "- методом Зейделя или методом релаксации.",
-            "Сравнить количество итераций.",
+            "Сравнить количество итераций.",  # <----------------------------------------------
             "Находить решения с разной точностью (т.е. варьировать ε, до достижения которого проводятся итерации).",
             "Может быть между ε и количеством итераций k есть зависимость?",
             "Примечание: поскольку релаксационные методы используют",
             "более “свежую” информацию о сделанных преобразованиях,",
-            "то обычно они сходятся быстрее метода простой итерации.",
+            "то обычно они сходятся быстрее метода простой итерации.",  # <------------------------
             "Протестировать работу методов на плохо обусловленных матрицах —",
             "например, на примере из методички А.Н.Пакулиной и на матрице Гильберта (см. задание 1).",
             "Если есть возможность — протестировать работу методов на",
@@ -27,28 +27,36 @@ def calc_answer(params: dict):
     A = np.array(params['A'])
     b = np.array(params['b'])
     epsilon = float(params['epsilon'])
-    X, comment = solve_simple(A, b, epsilon, verbosity=1)
+    output = {}
+    X = solve_simple(A, b, epsilon, output=output)
     X_ = np.linalg.solve(A, b)
-    diff = compute_diff(X, X_)
+    diff = calc_diff(X, X_)
     answer = {
         # NaNs are unexpected characters when dumping to json
         "X": str(X.tolist()),
         "X, полученный библиотечной функцией": str(X_.tolist()),
         "|X - X_bib|": str(diff),
-        "comment": comment,
     }
+    answer.update(output)
     return json.dumps(answer, ensure_ascii=False)
 
 
 def print_answer(A: np.array, b: np.array):
     epsilon = 10**(-10)
-    X = solve_simple(A, b, epsilon, verbosity=1)[0]
+    X = solve_simple(A, b, epsilon, verbosity=1)
     X_ = np.linalg.solve(A, b)
+    X_relax = solve_relax(
+        A, b, epsilon, [0.0 for i in range(len(A))], verbosity=1)
     print_diff(X, X_)
 
 
 def main():
     print_task(4)
+
+    print_test("Из 0, 1, 2")
+    A = np.array([[3.0, 1.0], [2.0, 3.0]])
+    b = np.array([1.0, 1.0])
+    print_answer(A, b)
 
     print_test("Гильберта 2-го порядка")
     A, b = SOLE.hilbert(2)
--- src/tasks/task04_lib.py
+++ src/tasks/task04_lib.py
@@ -1,7 +1,10 @@
+from cmath import inf, nan
 import numpy as np
 
+from src.tasks.task01_lib import calc_diff
 
-def compute_aposterior(alpha: np.array, x_k: np.array, x_k_minus_1: np.array):
+
+def calc_aposterior(alpha: np.array, x_k: np.array, x_k_minus_1: np.array):
     alpha_norm = np.linalg.norm(alpha)
     error = np.linalg.norm(x_k - x_k_minus_1) * \
         alpha_norm / abs(1 - alpha_norm)
@@ -9,7 +12,8 @@ def compute_aposterior(alpha: np.array, x_k: np.array, x_k_minus_1: np.array):
 
 
 def build_x_k(alpha: np.array, beta: np.array, x_k_minus_1: np.array):
-    return beta + np.dot(alpha, x_k_minus_1)
+    a = beta + np.dot(alpha, x_k_minus_1)
+    return a
 
 
 def build_alpha(A: np.array):
@@ -27,25 +31,111 @@ def build_beta(A: np.array, b: np.array):
     return np.array([b[i] / A[i][i] for i in range(n)])
 
 
-def solve_simple(A: np.array, b: np.array, epsilon: float, x0: np.array = None, verbosity: int = 0) -> tuple[np.array, str]:
+def solve_simple(A: np.array, b: np.array, epsilon: float, x0: np.array = None, verbosity: int = 0, output: dict = None) -> tuple[np.array, str]:
     alpha = build_alpha(A)
     beta = build_beta(A, b)
     if x0 is None:
         x0 = beta
     x_k_minus_1 = x0
     x_k = build_x_k(alpha, beta, x_k_minus_1)
-    error = compute_aposterior(alpha, x_k, x_k_minus_1)
+    error = calc_aposterior(alpha, x_k, x_k_minus_1)
     iter = 1
     while (error > epsilon):
         x_k_minus_1 = x_k
         x_k = build_x_k(alpha, beta, x_k_minus_1)
-        error = compute_aposterior(alpha, x_k, x_k_minus_1)
+        error = calc_aposterior(alpha, x_k, x_k_minus_1)
         iter += 1
     if any(np.isnan(x_k)):
         comment = f"Невозможно найти решение; ‖alpha‖ = {np.linalg.norm(alpha)}"
     else:
-        comment = f"Решение найдено для ε = {epsilon} за {iter} итераций." + \
+        comment = f"Решение найдено для ε = {epsilon} за {iter} итераций. " + \
             f"Апостериорная погрешность = {error}"
     if verbosity >= 1:
         print(comment)
-    return x_k, comment
+    else:
+        output.update({"Количество итераций": iter, "comment": comment})
+    return x_k
+
+
+# everywhere below:
+# i         — row number in A
+# n         — row number in x; often equals to i
+# x_U_k     — x^k
+# x_i_U_k   — x_i^k
+# j_0       — j to start summation with; numbering starts with 0 instead of 1 in the presentations
+# max_delta — container needed to populate up the current precision (accessed by ref)
+def calc_sum(A: np.array, x_U_k_minus_1: np.array, i: int, j_0: int):
+    result = np.dot(A[i][j_0:], x_U_k_minus_1[j_0:]).sum()
+    # print(f"sum_result={result}")
+    return result
+
+
+def calc_sum_of_summands(A: np.array, x_U_k: np.array, i: int, n: int):
+    if n <= 0:
+        return 0.0
+    # print(f"summands: x={x_U_k}")
+    # print(f"summands: A={A[i][0:n]}, x={x_U_k[0:n]}")
+    result = np.dot(A[i][0:n], x_U_k[0:n]).sum()
+    # print(f"summands_result={result}")
+    return result
+
+
+def calc_delta_i(A: np.array, b: np.array, x_U_k: np.array, x_U_k_minus_1: np.array, i: int, n: int):
+    delta = abs(calc_sum_of_summands(A, x_U_k, i, n) +
+                calc_sum(A, x_U_k_minus_1, i, n) - b[i])
+    # print(f"δ={delta}, sum={calc_sum(A, x_U_k_minus_1, i, n)}, summands={calc_sum_of_summands(A, x_U_k, i, n)} when i={i}, n={n}")
+    return delta
+
+
+def select_i(A: np.array, b: np.array, x_U_k: np.array, x_U_k_minus_1: np.array, n: int, i_to_ignore: list):
+    i_for_max_delta = 0
+    max_delta = 0.0
+    for i in [j for j in range(len(A)) if j not in i_to_ignore]:
+        delta_i = calc_delta_i(A, b, x_U_k, x_U_k_minus_1, i, n)
+        if (delta_i > max_delta):
+            max_delta = delta_i
+            i_for_max_delta = i
+    # print(f"i_for_max_δ={i_for_max_delta} when n={n}")
+    return i_for_max_delta
+
+
+# compute and not calc, because it modifies i_to_ignore
+def compute_x_n_U_k(A: np.array, b: np.array, x_U_k: np.array, x_U_k_minus_1: np.array, n: int, i_to_ignore: list, max_delta: list):
+    i = select_i(A, b, x_U_k, x_U_k_minus_1, n, i_to_ignore)
+    i_to_ignore.append(i)
+    max_delta.clear()
+    max_delta.append(calc_delta_i(A, b, x_U_k, x_U_k_minus_1, i, n))
+    sum = calc_sum(A, x_U_k_minus_1, i, n+1)
+    summands = calc_sum_of_summands(A, x_U_k, i, n)
+    # print(f"sum={sum}, summands={summands}")
+    x = (b[i] - sum -
+         summands) / A[i][n]
+    # print(f"x_n={x} when n={n}")
+    return x
+
+
+def build_x_U_k(A: np.array, b: np.array, x_k_minus_1: np.array, max_delta: list):
+    x_U_k = np.array([0.0 for n in range(len(A))])
+    i_to_ignore = []
+    for n in range(len(A)):
+        x_U_k[n] = compute_x_n_U_k(A, b, x_U_k, x_k_minus_1,
+                                   n, i_to_ignore, max_delta)
+    return x_U_k
+
+
+def solve_relax(A: np.array, b: np.array, epsilon: float, x_U_0: np.array = None, verbosity: int = 0, output: dict = None) -> tuple[np.array, str]:
+    if x_U_0 is None:
+        x_U_0 = build_beta(A, b)
+    max_delta = []
+    x_U_k_minus_1 = x_U_0
+    x_U_k = build_x_U_k(A, b, x_U_0, max_delta)
+    # print(f"max_δ={max_delta}")
+    # print(f"x^k={x_U_k}")
+    while (max_delta[0] > epsilon and not (any(np.isnan(x_U_k)) or any(np.isinf(x_U_k)))):
+        x_U_k_minus_1 = x_U_k
+        x_U_k = build_x_U_k(A, b, x_U_k_minus_1, max_delta)
+        # print(f"max_δ={max_delta}")
+        if (any(np.isnan(x_U_k)) or any(np.isinf(x_U_k))):
+            break
+    print(f"x^k={x_U_k}")
+    return x_U_k
